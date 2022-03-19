7b97137abc6ed84af23cd374cf6c586c3410a48c
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-03-20 02:10:49 +0900

    FacebookCloneApp: main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:facebook_clone_app/config/palette.dart';
+import 'package:facebook_clone_app/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,15 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Facebook Clone App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Palette.scaffold,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Main Screen'),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
